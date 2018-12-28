f6c580775cbc6a255ce4df307c368e64ee189ba4
Marie Burns <mcburns2222@gmail.com>
2018-12-28 00:17:24 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -18,7 +18,7 @@ class Doctor
   end
   
   def appointments
-    Appointment.all.collect do |appointment|
+    Appointment.all.select do |appointment|
       appointment.doctor == self
     end
   end
